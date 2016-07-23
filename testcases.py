2816fe70607c953e888c83582fe5c8ba40394d60
Santiago0076 <santiago007roman@hotmail.com>
2016-07-23 20:34:30 +0000

    Adding files

--- testcases.py
+++ testcases.py
@@ -10,6 +10,13 @@ class MyTest(unittest.TestCase):
         n.b = 2
         self.assertNotEqual(n.b, 3)
 
+    def test_is_non_values(self):
+        n = exer.MyDict()
+        n.a = 1
+        n.b = 2
+        self.assertNotEqual(n.a, 'Bananas')
+        self.assertNotEqual(n.b, 'Cherrys')
+
 
 
 if __name__ == '__main__':
